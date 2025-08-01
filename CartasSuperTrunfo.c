09c89c6386423b80caa24b5a8e9cf82371c06e1d
Rudsonsantoss <rudson100fiel@gmail.com>
2025-08-01 23:50:31 +0000

    desafios do super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,22 +4,22 @@ int main() {
 printf("Desafio de Cartas Super Trunfo!\n");
 
 printf("Carta 1\n");
-char estado = 'C';
-char codigo [4] = "R01";
-char cidade [10] = "Goianinha";
-int populacao = 10000;
-float area_em_km2 = 192.279;
-float pib = 546.576;
-int pontos_Turisticos = 10;
+char estado;
+char codigo [5];
+char cidade [30];
+int populacao;
+float area_em_km2;
+float pib;
+int pontos_Turisticos;
 
 printf("Estado: \n");
 scanf(" %c", &estado);
 
 printf("Código: \n");
-scanf("%s", &codigo);
+scanf("%s", codigo);
 
 printf("Nome da cidade: \n");
-scanf("%s", &cidade);
+scanf("%s", cidade);
 
 printf("População: \n");
 scanf("%d", &populacao);
@@ -30,18 +30,38 @@ scanf("%f",&area_em_km2);
 printf("PIB: \n");
 scanf("%f", &pib);
 
-printf("Pontos Túriticos: \n");
-scanf("%f", pontos_Turisticos);
+printf("Pontos Turísticos: \n");
+scanf("%d", &pontos_Turisticos);
 
 printf("carta 2\n");
-char estado2 = 'P';
-char codigo2 [4] = "P02";
-char cidade2 [10] = "recife";
-int populacao2 = 159000;
-float area_em_km2_2 = 230.358;
-float Pib2 = 650.567;
-int pontos_Turisticos2 = 35;
+char estado2;
+char codigo2 [5];
+char cidade2 [30];
+int populacao2;
+float area_em_km2_2;
+float pib2;
+int pontos_Turisticos2;
 
+printf("Estado: \n");
+scanf(" %c", &estado2);
+
+printf("Código: \n");
+scanf("%s", codigo2);
+
+printf("Nome da cidade: \n");
+scanf("%s", cidade2);
+
+printf("População: \n");
+scanf("%d", &populacao2);
+
+printf("Àrea em km²: \n");
+scanf("%f",&area_em_km2_2);
+
+printf("PIB: \n");
+scanf("%f", &pib2);
+
+printf("Pontos Turísticos: \n");
+scanf("%d", &pontos_Turisticos2);
 
 
 
